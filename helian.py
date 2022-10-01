5a6578eef087988ddf3e1dcf812850feb9208c1e
MartinPepa <martin.paredes.ross@mi.unc.edu.ar>
2022-10-01 11:16:31 -0300

    primerCommit

---
+++ helian.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 20 16:09:29 2022
+
+@author: Luis Martín Paredes Ross
+"""
+
+"""
+#%% Subrutina de interpolacion splin
+
+# La variable independiente debe estar ordenada en valores crecientes
+# Dimension del problama principal
+# Dimension x(n), s(n), z(n), a(2n-s)
+
+def splin(n,x,s,z,a):
+    #...
+    # Construccion del sistema de ecuaciones
+    #...
+    # Construccion y solucion del sistema auxiliar
+    #...
+    # Solucion definitiva del sistema
+    #...
+    # Calculo de la segunda derivada en los nodos
+    return True
+
+#%% Coeficientes de la funcion splin de tercer grado en el intervalo Y
+
+def specoe(xi,xj,si,sj,zi,zj,c):
+    #...
+    return True
+#%% Integral de la funcion splin entre A y B
+
+def spint(n,x,s,z,A,B):
+    #...
+    # Llama a specoe
+    return True
+
+#%% Calculo de la funcion splin en el punto Y
+
+def spfun(n,x,s,z,Y):
+    #...
+    # Llama a specoe
+    return True
+
+#%% Calculo de la derivada de la funcion splin en el punto Y
+
+def spder(n,x,s,z,a):
+    #...
+    # Llama a specoe
+    return True
+"""
+
+#%% Lectura de datos
+
+
+#%%
+
+if alf_per > alf_max:
+    alf_per = alf_max
+    R       = D/2
+    Omega   = pi*rpm/30
+    v_t     = Omega*R
+    rho     = 0.1249*(1-2.25577/100000*h)^(4.25575)
+    Temp    = 15.0 - 0.065*h
+    mu      = 1.7894e-5 * ((Temp/15.0)^0.75)
+    nu      = mu/rho
+    alf_mx  = alfa_max*pi/180
+    splin(ndim1,xd,beta,z,a)
+    be75    = spfun(ndim1,xd,beta,z,0.75)
+    
+    ajtov   = D*rpm/60.0
+    vei     = aji*ajtov
+    nve     = nj+1
+    delbe0  = bta75i - be75/pi*180.0
+    delbei  = (bta75f-bta75i)/nbta
+    ndelbe  = nbta+1
+#%%
+
+
+
+
+
+
+
+#%%
+
+
+
+
+
+
+#%%
+
+
+
+
+
+
---
+++ spline_3_grado.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 21:34:41 2022
+
+@author: WinUser
+"""
+
+import numpy as np
+from scipy.interpolate import CubicSpline
+import matplotlib.pyplot as plt
+import csv
+
+def lectura_helice(archivo_helice):
+    with open(archivo_helice, 'rt', encoding = 'utf8') as f:
+        rows        = csv.reader(f)
+        fila_1      = next(rows)
+        fila_2      = next(rows)
+        titulo      = str(fila_2[0])
+        Ndim1       = int(fila_2[1])
+        encabezado  = next(rows)
+        xD          = []
+        csR         = []
+        beta        = []
+        for i, row in enumerate(rows):
+            xD.append(float(row[0]))
+            csR.append(float(row[1]))
+            beta.append(float(row[2]))
+        return titulo, Ndim1, xD, csR, beta
+    
+def lectura_curva_polar(archivo_polar):
+    with open(archivo_polar, 'rt', encoding = 'utf8') as f:
+        rows        = csv.reader(f)
+        fila_1      = next(rows)
+        fila_2      = next(rows)
+        titulo      = str(fila_2[0])
+        Ndim2       = int(fila_2[1])
+        encabezado  = next(rows)
+        alfa        = []
+        cl          = []
+        cd          = []
+        for i, row in enumerate(rows):
+            alfa.append(float(row[0]))
+            cl.append(float(row[1]))
+            cd.append(float(row[2]))
+        return titulo, Ndim2, alfa, cl, cd
+
+def lectura_simulacion(parametros):
+    with open(parametros, 'rt', encoding = 'utf8') as f:
+        rows = csv.reader(f)
+        datos = []
+        for i, row in enumerate(rows):
+            datos.append(row[1])
+        titulo  = datos[0]
+        AJ0     = float(datos[1])
+        AJf     = float(datos[2])
+        NJ      = float(datos[3])
+        rpm     = float(datos[4])
+        h       = float(datos[5])
+        B       = float(datos[6])
+        D       = float(datos[7])
+        X0      = float(datos[8])
+        DeltaX  = float(datos[9])
+        NX      = float(datos[10])
+        Beta75i = float(datos[11])
+        Beta75f = float(datos[12])
+        NBeta   = float(datos[13])
+        AlfaMin = float(datos[14])
+        AlfaPer = float(datos[15])
+        AlfaMax = float(datos[16])
+        Impres  = float(datos[17])
+        return titulo, AJ0, AJf, NJ, rpm, h, B, D, X0, DeltaX, NX, Beta75i,\
+            Beta75f, NBeta, AlfaMin, AlfaPer, AlfaMax, Impres
+
+
+titulo1, NDim1, xD, csR, beta = lectura_helice('helice.csv')
+titulo2, NDim2, alfa, cl, cd = lectura_curva_polar('polar.csv')
+
+# x = np.linspace(0.2,0.95,num=16,retstep=False)
+# csr = [0.1180,0.1250,0.1290,0.1320,0.1340,0.1340,0.1320,0.1300,0.1260,0.1200,0.1140,0.1050,0.0950,0.0850,0.0720,0.0600]
+# beta = [0.4192,0.3759,0.3321,0.2916,0.2642,0.2293,0.2040,0.1831,0.1630,0.1435,0.1289,0.1183,0.1090,0.0989,0.0917,0.0836]
+cs1 = CubicSpline(xD, csR)
+cs2 = CubicSpline(xD, beta)
+
+# xs = np.arange(0.2,1.0,0.05)
+
+fig, ax = plt.subplots(dpi=400)
+
+ax.plot(xD, csR    , 'o', label = 'cuerda/radio')
+ax.plot(xD, beta   , 'x', label = 'beta [rad]')
+ax.plot(xD, cs1(xD), label = 'spline csr')
+ax.plot(xD, cs2(xD), label = 'spline beta')
+
+ax.set_xlim(0.0,1.0)
+ax.legend(loc='upper right',ncol=1)
+
+plt.show()
+
+cs3 = CubicSpline(alfa, cl)
+cs4 = CubicSpline(alfa, cd)
+
+fig, bx = plt.subplots(dpi=400)
+
+bx.plot(alfa, cl, 'o', label = 'cl-alfa')
+bx.plot(alfa, cd, 'x', label = 'cd-alfa')
+bx.plot(alfa, cs3(alfa),label = 'spline cl')
+bx.plot(alfa, cs4(alfa),label = 'spline cd')
+
+bx.legend(loc='upper left',ncol=1)
+plt.show()
+
+fig, cx = plt.subplots(dpi=400)
+cx.plot(cd, cl, 'x', label = 'polar')
+plt.show()
+
+titulo, AJ0, AJf, NJ, rpm, h, B, D, X0, DeltaX, NX, Beta75i, Beta75f, NBeta,\
+    AlfaMin, AlfaPer, AlfaMax, Impres = lectura_simulacion('parametros.csv')
